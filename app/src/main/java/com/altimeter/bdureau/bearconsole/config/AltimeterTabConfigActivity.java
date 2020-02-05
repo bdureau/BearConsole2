3eef6c2fa964081067bcd0bba48118a84be07cc8
BDUREAU <boris.dureau@neuf.fr>
2020-02-05 13:02:58 +0100

    fixes in the config
    Signed-off-by: BDUREAU <boris.dureau@neuf.fr>

--- app/src/main/java/com/altimeter/bdureau/bearconsole/config/AltimeterTabConfigActivity.java
+++ app/src/main/java/com/altimeter/bdureau/bearconsole/config/AltimeterTabConfigActivity.java
@@ -21,6 +21,7 @@ import android.view.View;
 import android.view.ViewGroup;
 //import com.google.android.material.tabs.TabLayout;
 
+
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
