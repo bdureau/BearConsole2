56b98d8fd9194d61829253ab2e0b31d17590db15
bdureau <boris.dureau@neuf.fr>
2021-03-21 23:32:51 +0100

    Release candidate 5
    Signed-off-by: bdureau <boris.dureau@neuf.fr>

--- app/src/main/java/com/altimeter/bdureau/bearconsole/Help/AboutActivity.java
+++ app/src/main/java/com/altimeter/bdureau/bearconsole/Help/AboutActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.altimeter.bdureau.bearconsole.BuildConfig;
 import com.altimeter.bdureau.bearconsole.ConsoleApplication;
 import com.altimeter.bdureau.bearconsole.R;
 
@@ -16,6 +18,7 @@ import com.altimeter.bdureau.bearconsole.R;
 public class AboutActivity extends AppCompatActivity {
 
     Button btnDismiss;
+    TextView txtViewVersion;
     ConsoleApplication myBT;
 
     @Override
@@ -29,7 +32,8 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         btnDismiss = (Button)findViewById(R.id.butDismiss);
-
+        txtViewVersion = (TextView) findViewById(R.id.txtViewVersion);
+        txtViewVersion.setText(BuildConfig.VERSION_NAME);
         btnDismiss.setOnClickListener(new View.OnClickListener()
         {
             @Override
