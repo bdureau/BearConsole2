e3e71cbdb33da4aa48f84f255e8680d5cb8c8b61
bdureau <boris.dureau@neuf.fr>
2021-03-27 00:02:38 +0100

    Release candidate 6
    Signed-off-by: bdureau <boris.dureau@neuf.fr>

--- app/src/main/java/com/altimeter/bdureau/bearconsole/FlashFirmware.java
+++ app/src/main/java/com/altimeter/bdureau/bearconsole/FlashFirmware.java
@@ -49,8 +49,8 @@ public class FlashFirmware extends AppCompatActivity {
     private ArrayList<Boards> mBoardList;
     private UartConfig uartConfig;
 
-    private static final String ASSET_FILE_NAME_ALTIMULTIV2       = "firmwares/2021-03-21-V1_22.altimultiV2.hex";
-    private static final String ASSET_FILE_NAME_ALTIMULTI         = "firmwares/2021-03-21-V1_22.altimulti.hex";
+    private static final String ASSET_FILE_NAME_ALTIMULTIV2       = "firmwares/2021-03-26-V1_24.altimultiV2.hex";
+    private static final String ASSET_FILE_NAME_ALTIMULTI         = "firmwares/2021-03-26-V1_24.altimulti.hex";
     private static final String ASSET_FILE_NAME_ALTISERVO         = "firmwares/2021-03-21-AltiServoV1_2";
     private static final String ASSET_FILE_NAME_ALTIDUO         = "firmwares/2021-03-21-V1_7.AltiDuo.hex";
 
