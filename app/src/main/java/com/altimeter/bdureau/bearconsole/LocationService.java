3bf9d5f0a8d5ddde153859e0b3f1a6d82a98ba3f
bdureau <boris.dureau@neuf.fr>
2022-01-02 18:33:39 +0100

    Fixed rocket track
    Signed-off-by: bdureau <boris.dureau@neuf.fr>

---
+++ app/src/main/java/com/altimeter/bdureau/bearconsole/LocationService.java
@@ -0,0 +1,59 @@
+package com.altimeter.bdureau.bearconsole;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class LocationService extends Service {
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationCallback locationCallback;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                Intent intent = new Intent("ACT_LOC");
+                intent.putExtra("latitude", locationResult.getLastLocation().getLatitude());
+                intent.putExtra("longitude", locationResult.getLastLocation().getLongitude());
+                sendBroadcast(intent);
+            }
+
+        };
+
+
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        requestLocation();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void requestLocation() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.myLooper());
+    }
+}
