f25b41852c624e22c9a2ccf03afe535c2eb56ba3
bdureau <boris.dureau@neuf.fr>
2022-03-25 07:56:35 +0100

    Fixes to the whatsapp share for Android 10
    Signed-off-by: bdureau <boris.dureau@neuf.fr>

--- app/src/main/java/com/altimeter/bdureau/bearconsole/ShareHandler.java
+++ app/src/main/java/com/altimeter/bdureau/bearconsole/ShareHandler.java
@@ -5,14 +5,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
 public class ShareHandler {
 
-    public static Bitmap takeScreenshot(View rootView) {
+    /*public static Bitmap takeScreenshot(View rootView) {
         //View rootView = view.findViewById(android.R.id.content).getRootView();
         rootView.setDrawingCacheEnabled(true);
         return rootView.getDrawingCache();
@@ -30,5 +39,58 @@ public class ShareHandler {
         shareIntent.putExtra(Intent.EXTRA_TEXT, "");
         shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
         ctx.startActivity(Intent.createChooser(shareIntent, "BearConsole has shared you some info"));
+    }*/
+
+    public static  void takeScreenShot(View view, Context ctx) {
+        Date date = new Date();
+        CharSequence format = DateFormat.format("MM-dd-yyyy_hh:mm:ss", date);
+
+        try {
+            File mainDir = new File(
+                    ctx.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "FilShare");
+            if (!mainDir.exists()) {
+                boolean mkdir = mainDir.mkdir();
+            }
+
+            String path = mainDir + "/" + "AltiMultiCurve" + "-" + format + ".jpeg";
+            view.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+            view.setDrawingCacheEnabled(false);
+
+
+            File imageFile = new File(path);
+            FileOutputStream fileOutputStream = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            shareScreenShot(imageFile, ctx);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    //Share ScreenShot
+    public static  void shareScreenShot(File imageFile,Context ctx ) {
+
+        Log.d("Package Name", "Package Name" + ctx.getPackageName());
+        Uri uri = FileProvider.getUriForFile(
+                ctx,
+                ctx.getPackageName() +  ".provider",
+                imageFile);
+
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setType("image/*");
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, "Altimulti has shared with you some info");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        try {
+            ctx.startActivity(Intent.createChooser(intent, "Share With"));
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(ctx, "No App Available", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
